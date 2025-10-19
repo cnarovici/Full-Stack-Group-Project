8e1d13c277c7dd8604f8fa42da5cb1a7ccd82a8b
Tommy Kang <tommyk8477@gmail.com>
2025-10-19 01:55:44 -0500

    Folder removal (mispush error)

--- Full-Stack-Group-Project/frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LoginPage from './LoginPage';
-//Login Page
-function App() {
-  return <LoginPage />;
-}
-
-export default App;
--- Full-Stack-Group-Project/frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './LoginPage.css'; // use our CSS instead
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
